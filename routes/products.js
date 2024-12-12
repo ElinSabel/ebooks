021adacf6a1c2106e6d4b3c238d63fa17e97d625
Elin Sabel <Elin.sabel@live.se>
2024-12-12 12:08:22 +0100

    Add Search Compability

--- routes/products.js
+++ routes/products.js
@@ -11,5 +11,34 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET all products in search field
+router.get('/', async (req, res) => {
+    try {
+        const { name } = req.query;  // Extract the search parameter from query string
+        let products;
+
+        if (name) {
+            // If a search name is provided, find products matching the name (case-insensitive)
+            products = await Product.find({ name: { $regex: name, $options: 'i' } });
+        } else {
+            // If no search parameter, return all products
+            products = await Product.find();
+        }
+/* 
+        res.status(200).json(products); */
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// GET the choosen category 
+/* router.get('/category/:category', async (req, res) => {
+    // res.json(({message: req.params.category}))
+    try {
+        res.json(await Product.find(req.params.productId));
+    } catch(error) {
+        res.json({message: error});
+    }
+}); */
 
 module.exports = router;
